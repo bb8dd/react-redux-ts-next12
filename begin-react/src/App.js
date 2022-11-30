d99d039319112cfa44286c058cdf644275405408
bbb8ddd <ldh19971027@gmail.com>
2022-11-30 23:49:15 +0900

    props 구조분해 할당

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,8 +2,33 @@ import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: 'Car',
+      amount: 300,
+      date: new Date().toISOString(),
+    },
+    {
+      id: 2,
+      title: 'Phone',
+      amount: 100,
+      date: '2022. 12. 1',
+    },
+    {
+      id: 3,
+      title: 'Watch',
+      amount: 200,
+      date: '2022. 11. 30',
+    },
+  ];
+
   return (
-    <ExpenseItem />
+    <div>
+      <ExpenseItem data={data[0]} />
+      <ExpenseItem data={data[1]} />
+      <ExpenseItem data={data[2]} />
+    </div>
   );
 }
 
--- begin-react/src/components/ExpenseItem.js
+++ begin-react/src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-export default function ExpenseItem() {
-  const expenseDate = new Date(2022, 3, 29);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+export default function ExpenseItem({ data }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{data.title}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{data.date}</h2>
         <div className="expense-item__price">
           $
-          {expenseAmount}
+          {data.mount}
         </div>
       </div>
     </div>
