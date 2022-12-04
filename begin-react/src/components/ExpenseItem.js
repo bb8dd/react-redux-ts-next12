70a2df637a2a3b242d774ceb0eed327861605a5f
bbb8ddd <ldh19971027@gmail.com>
2022-12-04 22:55:51 +0900

    컴포넌트 나누기

--- begin-react/src/components/ExpenseItem.js
+++ begin-react/src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 export default function ExpenseItem({ data }) {
-  const year = data.date.getFullYear();
-  const month = data.date.toLocaleString('ko-KR', { month: 'long' });
-  const day = data.date.toLocaleString('ko-KR', { day: '2-digit' });
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={data.date} />
       <div className="expense-item__description">
         <h2>{data.title}</h2>
         <div className="expense-item__price">
