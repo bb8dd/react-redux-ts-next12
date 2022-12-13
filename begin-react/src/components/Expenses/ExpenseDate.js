71b1a73a16efd74adf42401b0d80cf2ee3bafb29
bbb8ddd <ldh19971027@gmail.com>
2022-12-13 23:11:09 +0900

    folder 나누기

--- begin-react/src/components/Expenses/ExpenseDate.js
+++ begin-react/src/components/Expenses/ExpenseDate.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './ExpenseDate.css';
 
 function ExpenseDate({ date }) {
-  console.log(date);
   const year = date.getFullYear();
   const month = date.toLocaleString('ko-KR', { month: 'long' });
   const day = date.toLocaleString('ko-KR', { day: '2-digit' });
